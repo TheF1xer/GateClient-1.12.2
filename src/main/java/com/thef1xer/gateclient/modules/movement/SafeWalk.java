6c9b120d8e286c3c96a898366290b28c32698ec4
TheF1xer <66278651+TheF1xer@users.noreply.github.com>
2021-06-21 23:49:57 -0400

    Safewalk bugfixes

--- src/main/java/com/thef1xer/gateclient/modules/movement/SafeWalk.java
+++ src/main/java/com/thef1xer/gateclient/modules/movement/SafeWalk.java
@@ -34,28 +34,53 @@ public class SafeWalk extends Module {
     public void onMove(PlayerMoveEvent event) {
         Minecraft mc = Minecraft.getMinecraft();
         if (mc.player.onGround) {
+            if (this.mode.getCurrentValue() == Mode.STOP) {
 
-            if (mc.world.getCollisionBoxes(mc.player, mc.player.getEntityBoundingBox().offset(event.x, -1, event.z)).isEmpty()) {
-                boolean flag1 = true;
-                boolean flag2 = true;
-
-                if (this.mode.getCurrentValue() == Mode.STOP) {
-                    if (mc.world.getCollisionBoxes(mc.player, mc.player.getEntityBoundingBox().offset(event.x, -1, 0)).isEmpty()) {
+                while (mc.world.getCollisionBoxes(mc.player, mc.player.getEntityBoundingBox().offset(event.x, -1, 0)).isEmpty()) {
+                    if (event.x < 0.05D && event.x >= -0.05D) {
                         event.x = 0;
-                        flag1 = false;
+                    } else if (event.x > 0) {
+                        event.x -= 0.05D;
+                    } else {
+                        event.x += 0.05D;
                     }
+                }
 
-                    if (mc.world.getCollisionBoxes(mc.player, mc.player.getEntityBoundingBox().offset(0, -1, event.z)).isEmpty()) {
+                while (mc.world.getCollisionBoxes(mc.player, mc.player.getEntityBoundingBox().offset(0, -1, event.z)).isEmpty()) {
+                    if (event.z < 0.05D && event.z >= -0.05D) {
                         event.z = 0;
-                        flag2 = false;
+                    } else if (event.z > 0) {
+                        event.z -= 0.05D;
+                    } else {
+                        event.z += 0.05D;
                     }
+                }
 
-                    if (flag1 && flag2) {
+                while (mc.world.getCollisionBoxes(mc.player, mc.player.getEntityBoundingBox().offset(event.x, -1, event.z)).isEmpty()) {
+                    if (event.x < 0.05D && event.x >= -0.05D) {
                         event.x = 0;
-                        event.z = 0;
+                    } else if (event.x > 0) {
+                        event.x -= 0.05D;
+                    } else {
+                        event.x += 0.05D;
                     }
-                } else {
+
+                    if (event.z < 0.05D && event.z >= -0.05D) {
+                        event.z = 0;
+                    } else if (event.z > 0) {
+                        event.z -= 0.05D;
+                    } else {
+                        event.z += 0.05D;
+                    }
+                }
+
+            } else {
+                if (mc.world.getCollisionBoxes(mc.player, mc.player.getEntityBoundingBox().offset(event.x, -1, 0)).isEmpty()
+                        || mc.world.getCollisionBoxes(mc.player, mc.player.getEntityBoundingBox().offset(0, -1, event.z)).isEmpty()
+                        || mc.world.getCollisionBoxes(mc.player, mc.player.getEntityBoundingBox().offset(event.x, -1, event.z)).isEmpty()) {
                     mc.player.movementInput.sneak = true;
+                    event.x *= 0.3D;
+                    event.z *= 0.3D;
                 }
             }
         }
