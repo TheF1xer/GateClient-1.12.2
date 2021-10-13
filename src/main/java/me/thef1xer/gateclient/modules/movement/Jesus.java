0a446ad2a1565dad12df486007cc0ecaf9e1e44c
TheF1xer <66278651+TheF1xer@users.noreply.github.com>
2021-10-12 22:50:14 -0300

    Jesus NCP bypass

--- src/main/java/me/thef1xer/gateclient/modules/movement/Jesus.java
+++ src/main/java/me/thef1xer/gateclient/modules/movement/Jesus.java
@@ -1,10 +1,17 @@
 package me.thef1xer.gateclient.modules.movement;
 
 import me.thef1xer.gateclient.events.GetLiquidCollisionBoundingBoxEvent;
+import me.thef1xer.gateclient.events.PacketEvent;
 import me.thef1xer.gateclient.modules.Module;
 import me.thef1xer.gateclient.settings.impl.FloatSetting;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockLiquid;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -14,6 +21,8 @@ public class Jesus extends Module {
     public static final Jesus INSTANCE = new Jesus();
 
     public final FloatSetting offset = new FloatSetting("Offset", "offset", 0.2F, 0F, 1F);
+
+    private final Minecraft mc = Minecraft.getMinecraft();
 
     public Jesus() {
         super("Jesus", "jesus", Module.ModuleCategory.MOVEMENT);
@@ -32,18 +41,56 @@ public class Jesus extends Module {
 
     @SubscribeEvent
     public void onLiquidCollision(GetLiquidCollisionBoundingBoxEvent event) {
-        if (Minecraft.getMinecraft().world == null || Minecraft.getMinecraft().player == null) {
+        if (mc.world == null || mc.player == null) {
             return;
         }
 
-        if (Minecraft.getMinecraft().player.isSneaking() || Minecraft.getMinecraft().player.fallDistance > 3F) {
+        if (mc.player.isSneaking() || mc.player.fallDistance > 3F || mc.player.motionY > 0.1F) {
             return;
         }
 
-        if (Minecraft.getMinecraft().player.isInWater() || Minecraft.getMinecraft().player.isInLava()) {
-            Minecraft.getMinecraft().player.motionY = 0.30000001192092896D;
+        if (mc.player.isInWater() || mc.player.isInLava()) {
+            mc.player.motionY = 0.135D;
             return;
         }
-        event.setCollisionBoundingBox(new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D - (double) offset.getValue(), 1.0D));
+        event.setCollisionBoundingBox(new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D - offset.getValue(), 1.0D));
+    }
+
+    @SubscribeEvent
+    public void onPacket(PacketEvent event) {
+        if (event.getPacket() instanceof CPacketPlayer.Position || event.getPacket() instanceof CPacketPlayer.PositionRotation) {
+            if (isWaterWalking() && !mc.player.isSneaking()) {
+                if (mc.player.ticksExisted % 2 == 0) {
+                    EntityPlayerSP player = mc.player;
+                    event.setPacket(new CPacketPlayer.PositionRotation(player.posX, player.posY + offset.getValue(),
+                            player.posZ, player.rotationYaw, player.rotationPitch, player.onGround));
+                }
+            }
+        }
+    }
+
+    private boolean isOverLiquid() {
+        if (mc.player != null) {
+            AxisAlignedBB bb = mc.player.getEntityBoundingBox().offset(0, - offset.getValue(), 0);
+
+            for (int x = MathHelper.floor(bb.minX); x < MathHelper.floor(bb.maxX) + 1; x++) {
+                for (int z = MathHelper.floor(bb.minZ); z < MathHelper.floor(bb.minZ) + 1; z++) {
+                    final Block block = mc.world.getBlockState(new BlockPos(x, bb.minY, z)).getBlock();
+
+                    if (!(block instanceof BlockLiquid)) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isWaterWalking() {
+        return !mc.player.isInWater()
+                && !mc.player.isInLava()
+                && isOverLiquid();
     }
 }
