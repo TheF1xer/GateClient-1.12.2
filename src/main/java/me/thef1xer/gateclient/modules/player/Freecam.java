f926a9525d739a3fe2a0ff70113776c4abb38154
TheF1xer <66278651+TheF1xer@users.noreply.github.com>
2021-12-23 02:38:31 -0300

    Freecam fixes and version update

--- src/main/java/me/thef1xer/gateclient/modules/player/Freecam.java
+++ src/main/java/me/thef1xer/gateclient/modules/player/Freecam.java
@@ -8,7 +8,10 @@ import me.thef1xer.gateclient.settings.impl.FloatSetting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityOtherPlayerMP;
 import net.minecraft.network.play.client.CPacketUseEntity;
+import net.minecraft.util.MovementInput;
+import net.minecraft.util.MovementInputFromOptions;
 import net.minecraft.util.math.Vec3d;
+import net.minecraftforge.client.event.InputUpdateEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -19,7 +22,7 @@ public class Freecam extends Module {
     public final FloatSetting verticalSpeed = new FloatSetting("Vertical Speed", "verticalspeed", 3F);
     public final FloatSetting horizontalSpeed = new FloatSetting("Horizontal Speed", "horizontalspeed", 3F);
 
-    private Minecraft mc = Minecraft.getMinecraft();
+    private final Minecraft mc = Minecraft.getMinecraft();
     private int lastThirdPerson;
     public EntityOtherPlayerMP camera;
     private boolean activeThisSession = false;
@@ -57,7 +60,7 @@ public class Freecam extends Module {
 
     @SubscribeEvent
     public void onTick(TickEvent.ClientTickEvent event) {
-        //TODO: Fix sneak and already existing movement inputs
+        //TODO: Try to fix weird hand movement that sometimes happens
 
         if (mc.world == null || !mc.world.isRemote) {
             camera = null;
@@ -78,16 +81,33 @@ public class Freecam extends Module {
         mc.gameSettings.thirdPersonView = 0;
         camera.inventory = mc.player.inventory;
         camera.setHealth(mc.player.getHealth());
+    }
 
-        float forward = (mc.player.movementInput.forwardKeyDown ? horizontalSpeed.getValue() : 0) -
-                (mc.player.movementInput.backKeyDown ? horizontalSpeed.getValue() : 0);
-        float strafe = (mc.player.movementInput.leftKeyDown ? horizontalSpeed.getValue() : 0) -
-                (mc.player.movementInput.rightKeyDown ? horizontalSpeed.getValue() : 0);
-        float vertical = (mc.player.movementInput.jump ? verticalSpeed.getValue() : 0) -
-                (mc.player.movementInput.sneak ? verticalSpeed.getValue() : 0);
+    @SubscribeEvent
+    public void onInputUpdate(InputUpdateEvent event) {
+        if (camera == null) {
+            return;
+        }
+
+        MovementInput input = event.getMovementInput();
+
+        float forward = (input.forwardKeyDown ? horizontalSpeed.getValue() : 0) -
+                (input.backKeyDown ? horizontalSpeed.getValue() : 0);
+        float strafe = (input.leftKeyDown ? horizontalSpeed.getValue() : 0) -
+                (input.rightKeyDown ? horizontalSpeed.getValue() : 0);
+        float vertical = (input.jump ? verticalSpeed.getValue() : 0) -
+                (input.sneak ? verticalSpeed.getValue() : 0);
 
         Vec3d vector = new Vec3d(strafe, vertical, forward).rotateYaw((float) -Math.toRadians(camera.rotationYaw));
         camera.setPositionAndRotationDirect(camera.posX + vector.x, camera.posY + vector.y, camera.posZ + vector.z, camera.rotationYaw, camera.rotationPitch, 3, false);
+
+        //This should stop the player from moving if Baritone isn't being used, needs some testing
+        if (input instanceof MovementInputFromOptions) {
+            mc.player.moveVertical = 0;
+            mc.player.moveStrafing = 0;
+            mc.player.moveForward = 0;
+            mc.player.setJumping(false);
+        }
     }
 
     @SubscribeEvent
--- src/main/java/me/thef1xer/gateclient/util/Reference.java
+++ src/main/java/me/thef1xer/gateclient/util/Reference.java
@@ -3,5 +3,5 @@ package me.thef1xer.gateclient.util;
 public class Reference {
     public static final String MODID = "gateclient";
     public static final String NAME = "Gate Client";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3";
 }
