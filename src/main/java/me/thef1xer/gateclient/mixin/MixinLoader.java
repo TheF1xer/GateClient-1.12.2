08aca534eda5f829a065f694e8611e6e027d6b90
TheF1xer <66278651+TheF1xer@users.noreply.github.com>
2022-01-19 23:45:33 -0300

    Mixins added, must refactor every ASM visitor into a mixin class

---
+++ src/main/java/me/thef1xer/gateclient/mixin/MixinLoader.java
@@ -0,0 +1,44 @@
+package me.thef1xer.gateclient.mixin;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.Mixins;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+
+public class MixinLoader implements IFMLLoadingPlugin {
+
+    public MixinLoader() {
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixins.gateclient.json");
+        MixinEnvironment.getDefaultEnvironment().setObfuscationContext("searge");
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
