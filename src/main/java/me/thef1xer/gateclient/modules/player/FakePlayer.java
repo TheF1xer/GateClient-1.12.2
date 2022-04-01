22e7f684c60f2478498dbbbccbfd582afb116db6
TheF1xer <66278651+TheF1xer@users.noreply.github.com>
2022-03-31 22:11:30 -0300

    FakePlayer now disables itself when the current world is unloaded

--- src/main/java/me/thef1xer/gateclient/modules/player/FakePlayer.java
+++ src/main/java/me/thef1xer/gateclient/modules/player/FakePlayer.java
@@ -4,6 +4,9 @@ import com.mojang.authlib.GameProfile;
 import me.thef1xer.gateclient.modules.Module;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityOtherPlayerMP;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class FakePlayer extends Module {
     public static FakePlayer INSTANCE = new FakePlayer();
@@ -19,6 +22,7 @@ public class FakePlayer extends Module {
     @Override
     public void onEnable() {
         super.onEnable();
+        MinecraftForge.EVENT_BUS.register(this);
 
         // Disable module if not in-game
         if (mc.player == null || mc.world == null) {
@@ -40,9 +44,15 @@ public class FakePlayer extends Module {
     @Override
     public void onDisable() {
         super.onDisable();
+        MinecraftForge.EVENT_BUS.unregister(this);
 
         if (fakePlayer != null) {
             mc.world.removeEntity(fakePlayer);
         }
+    }
+
+    @SubscribeEvent
+    public void onUnloadWorld(WorldEvent.Unload event) {
+        setEnabled(false);
     }
 }
