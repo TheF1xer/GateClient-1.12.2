f8171281fe6639638b47a45046acf480197c2ed2
TheF1xer <66278651+TheF1xer@users.noreply.github.com>
2022-05-12 20:05:01 -0400

    Version change

--- src/main/java/me/thef1xer/gateclient/util/Reference.java
+++ src/main/java/me/thef1xer/gateclient/util/Reference.java
@@ -3,5 +3,5 @@ package me.thef1xer.gateclient.util;
 public class Reference {
     public static final String MODID = "gateclient";
     public static final String NAME = "Gate Client";
-    public static final String VERSION = "1.5";
+    public static final String VERSION = "1.5.1";
 }
